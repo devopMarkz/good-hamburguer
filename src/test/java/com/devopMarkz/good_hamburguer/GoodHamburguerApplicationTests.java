ff9c1fa20a643021343ae326e8384c7df09c7dd2
devopMarkz <marcosdev2002@gmail.com>
2026-05-02 20:35:36 -0300

    first commit

---
+++ src/test/java/com/devopMarkz/good_hamburguer/GoodHamburguerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopMarkz.good_hamburguer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodHamburguerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
